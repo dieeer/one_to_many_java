7cfc87255ede1444486e415511ff5a3b0061b9ec
justin reid <hi@justinre.id>
2022-10-25 10:11:27 +0100

    models complete

---
+++ src/main/java/com/example/onetomanylab/OnetomanylabApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetomanylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetomanylabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetomanylabApplication.class, args);
+	}
+
+}
---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,75 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "secondname")
+    private String secondName;
+
+    @Column(name = "employeenumber")
+    private int employeeNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "departmentId", nullable = true)
+    private Department department;
+
+    public Employee(String firstName, String secondName, int employeeNumber, Department department){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.employeeNumber = employeeNumber;
+        this.department = department;
+
+    }
+
+    public Employee(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
